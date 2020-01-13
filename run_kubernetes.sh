d452cddedb80de2e26bf0f11f6339a5dffb849b3
Richard Howes <richardh@workflowgroup.com>
2020-01-14 00:05:40 +0200

    Got as far as a running kubernetes cluster (Task 6: Deploy with Kubernetes and Save Output Logs). The section 'Make a prediction' not working

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="richardhowes/api:latest"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run apitest --image=$dockerpath --port=80 --labels="tag=udacity"
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+POD_NAME=$(kubectl get pods -l tag=udacity -o jsonpath='{.items[0].metadata.name}')
+kubectl port-forward "${POD_NAME}" 8000:80
